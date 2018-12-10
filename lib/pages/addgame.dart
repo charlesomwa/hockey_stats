92520ef452566da12ce1f4f6fb34c42211ca0fcd
Randy Kinne <r27kinne@gmail.com>
2018-12-10 16:16:23 -0500

    Begin formatting for AddGame page

--- lib/pages/addgame.dart
+++ lib/pages/addgame.dart
@@ -38,7 +38,73 @@ class _AddGame extends State<AddGame> {
               color: Colors.black12
           ),*/
 
-          SimpleButton(
+          Container(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: new Column(
+
+              children: <Widget>[
+                TextField(style: Theme.of(context).textTheme.body1, decoration: InputDecoration(hintText: "Game Name"),)
+              ],
+            ),
+          ),
+
+          SimpleIconButton(
+            backgroundColor: Theme.of(context).accentColor,
+            buttonText: Text("1 Goal(s)", style: Theme.of(context).textTheme.button),
+            //icons: [IconButton(icon: Icon(Icons.account_circle)), IconButton(icon: Icon(Icons.calendar_view_day))],
+            icons: [
+              Container(
+                padding: EdgeInsets.only(right: 90),
+                child: Text("-", style: Theme.of(context).textTheme.button.apply(fontSizeFactor: 1.5),),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 90),
+                child: Icon(Icons.add),
+              ),
+            ],
+            onPressed: clickButton,
+
+          ),
+
+          SimpleIconButton(
+            backgroundColor: Theme.of(context).accentColor,
+            buttonText: Text("3 Assist(s)", style: Theme.of(context).textTheme.button),
+            //icons: [IconButton(icon: Icon(Icons.account_circle)), IconButton(icon: Icon(Icons.calendar_view_day))],
+            icons: [
+              Container(
+                padding: EdgeInsets.only(right: 80),
+                child: Text("-", style: Theme.of(context).textTheme.button.apply(fontSizeFactor: 1.5),),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 80),
+                child: Icon(Icons.add),
+              ),
+            ],
+            onPressed: clickButton,
+
+          ),
+
+          SimpleIconButton(
+            backgroundColor: Theme.of(context).accentColor,
+            buttonText: Text("1 Penalty(ies)", style: Theme.of(context).textTheme.button),
+            //icons: [IconButton(icon: Icon(Icons.account_circle)), IconButton(icon: Icon(Icons.calendar_view_day))],
+            icons: [
+              Container(
+                padding: EdgeInsets.only(right: 70),
+                child: Text("-", style: Theme.of(context).textTheme.button.apply(fontSizeFactor: 1.5),),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 70),
+                child: Icon(Icons.add),
+              ),
+            ],
+            onPressed: clickButton,
+
+          )
+
+
+
+          /*SimpleButton(
             backgroundColor: Theme.of(context).accentColor,
             buttonText: Text("ADD NEW GAME",
                 style: Theme.of(context).textTheme.button
@@ -46,16 +112,9 @@ class _AddGame extends State<AddGame> {
 
             onPressed: clickButton,
             textColor: Colors.white,
-          ),
+          ),*/
 
-          Column(
-            children: <Widget>[
-              Text("Hello",
-                style: Theme.of(context).textTheme.title
-              )
 
-            ],
-          )
         ],
       )
   );
--- lib/util/SimpleButton.dart
+++ lib/util/SimpleButton.dart
@@ -4,13 +4,11 @@ class SimpleButton extends StatelessWidget {
 
   final Color backgroundColor;
   final Text buttonText;
-  final Color textColor;
   final Function onPressed;
 
   SimpleButton({
     this.backgroundColor,
     this.buttonText,
-    this.textColor,
     this.onPressed,
   });
 
@@ -35,6 +33,57 @@ class SimpleButton extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
                     child: buttonText,
                   ),
+                ],
+              ),
+              onPressed: () => onPressed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SimpleIconButton extends StatelessWidget {
+
+  final Color backgroundColor;
+  final Text buttonText;
+  final Function onPressed;
+  final List<Widget> icons;
+
+  SimpleIconButton({
+    this.backgroundColor,
+    this.buttonText,
+    this.onPressed,
+    this.icons = const [ Icon(Icons.edit) ],
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 15.0),
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: FlatButton(
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)),
+              splashColor: this.backgroundColor,
+              color: this.backgroundColor,
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  icons[0],
+
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    child: buttonText,
+                  ),
+
+                  icons[1]
                 ],
               ),
               onPressed: () => onPressed,
